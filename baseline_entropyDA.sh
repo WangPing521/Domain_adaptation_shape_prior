8d8a17fc6da503f1d48033a97a7b7100daa8e82f
WangPing521 <wp_scdedu@163.com>
2022-02-19 01:23:15 +0800

    entDAbash

---
+++ baseline_entropyDA.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set  -e -u -o pipefail
+
+CC_WRAPPER_PATH="CC_wrapper.sh"
+
+source $CC_WRAPPER_PATH
+
+time=3
+account=def-chdesa
+save_dir=baseline_entropyDA
+
+declare -a StringArray=(
+  "python main.py seed=123 Optim.lr=0.0001 Trainer.name=entda  DA.source=CT DA.target=MRI  Scheduler.RegScheduler.max_value=0.1 Trainer.save_dir=${save_dir}/CT2MRI_301lr_01reg"
+  "python main.py seed=123 Optim.lr=0.0001 Trainer.name=entda  DA.source=CT DA.target=MRI  Scheduler.RegScheduler.max_value=0.01 Trainer.save_dir=${save_dir}/CT2MRI_301lr_101reg"
+  "python main.py seed=123 Optim.lr=0.0001 Trainer.name=entda  DA.source=CT DA.target=MRI  Scheduler.RegScheduler.max_value=0.001 Trainer.save_dir=${save_dir}/CT2MRI_301lr_201reg"
+  "python main.py seed=123 Optim.lr=0.0001 Trainer.name=entda  DA.source=CT DA.target=MRI  Scheduler.RegScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/CT2MRI_301lr_301reg"
+
+
+  "python main.py seed=123 Optim.lr=0.00001 Trainer.name=entda DA.source=CT DA.target=MRI  Scheduler.RegScheduler.max_value=0.1 Trainer.save_dir=${save_dir}/CT2MRI_401lr_01reg"
+  "python main.py seed=123 Optim.lr=0.00001 Trainer.name=entda DA.source=CT DA.target=MRI  Scheduler.RegScheduler.max_value=0.01 Trainer.save_dir=${save_dir}/CT2MRI_401lr_101reg"
+  "python main.py seed=123 Optim.lr=0.00001 Trainer.name=entda DA.source=CT DA.target=MRI  Scheduler.RegScheduler.max_value=0.001 Trainer.save_dir=${save_dir}/CT2MRI_401lr_201reg"
+  "python main.py seed=123 Optim.lr=0.00001 Trainer.name=entda DA.source=CT DA.target=MRI  Scheduler.RegScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/CT2MRI_401lr_301reg"
+
+)
+
+for cmd in "${StringArray[@]}"
+do
+	echo ${cmd}
+	CC_wrapper "${time}" "${account}" "${cmd}" 16
+
+done
+
+
