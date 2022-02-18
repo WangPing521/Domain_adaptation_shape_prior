a184b0309b617504fe9056494a3908deb76f220b
WangPing521 <wp_scdedu@163.com>
2022-02-18 14:33:32 +0800

    mybaseline

--- baseline_test.sh
+++ baseline_test.sh
@@ -11,30 +11,13 @@ account=def-chdesa
 save_dir=baseline
 
 declare -a StringArray=(
-  "python main.py seed=123 Optim.lr=0.000001 Trainer.name=baseline DA.source=CT DA.target=MRI DataLoader.shuffle=True Trainer.save_dir=${save_dir}/CT2MRI_shuffleT_seed1"
-  "python main.py seed=213 Optim.lr=0.000001 Trainer.name=baseline DA.source=CT DA.target=MRI DataLoader.shuffle=True Trainer.save_dir=${save_dir}/CT2MRI_shuffleT_seed2"
-  "python main.py seed=321 Optim.lr=0.000001 Trainer.name=baseline DA.source=CT DA.target=MRI DataLoader.shuffle=True Trainer.save_dir=${save_dir}/CT2MRI_shuffleT_seed3"
+  "python main.py seed=123 Optim.lr=0.0001 Trainer.name=baseline DA.source=CT DA.target=MRI   Trainer.save_dir=${save_dir}/CT2MRI_301lr_seed1"
+  "python main.py seed=123 Optim.lr=0.00001 Trainer.name=baseline DA.source=CT DA.target=MRI  Trainer.save_dir=${save_dir}/CT2MRI_401lr_seed1"
+  "python main.py seed=123 Optim.lr=0.000001 Trainer.name=baseline DA.source=CT DA.target=MRI Trainer.save_dir=${save_dir}/CT2MRI_501lr_seed1"
 
-  "python main.py seed=123 Optim.lr=0.000001 Trainer.name=baseline DA.source=CT DA.target=MRI DataLoader.shuffle=False Trainer.save_dir=${save_dir}/CT2MRI_shuffleF_seed1"
-  "python main.py seed=213 Optim.lr=0.000001 Trainer.name=baseline DA.source=CT DA.target=MRI DataLoader.shuffle=False Trainer.save_dir=${save_dir}/CT2MRI_shuffleF_seed2"
-  "python main.py seed=321 Optim.lr=0.000001 Trainer.name=baseline DA.source=CT DA.target=MRI DataLoader.shuffle=False Trainer.save_dir=${save_dir}/CT2MRI_shuffleF_seed3"
-
-
-  "python main.py seed=123 Optim.lr=0.000001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=True Trainer.save_dir=${save_dir}/MRI2CT_shuffleT_seed1"
-  "python main.py seed=213 Optim.lr=0.000001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=True Trainer.save_dir=${save_dir}/MRI2CT_shuffleT_seed2"
-  "python main.py seed=321 Optim.lr=0.000001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=True Trainer.save_dir=${save_dir}/MRI2CT_shuffleT_seed3"
-
-  "python main.py seed=123 Optim.lr=0.000001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=False Trainer.save_dir=${save_dir}/MRI2CT_shuffleF_seed1"
-  "python main.py seed=213 Optim.lr=0.000001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=False Trainer.save_dir=${save_dir}/MRI2CT_shuffleF_seed2"
-  "python main.py seed=321 Optim.lr=0.000001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=False Trainer.save_dir=${save_dir}/MRI2CT_shuffleF_seed3"
-
-  "python main.py seed=123 Optim.lr=0.00001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=True Trainer.save_dir=${save_dir}/MRI2CT_401lr_shuffleT_seed1"
-  "python main.py seed=213 Optim.lr=0.00001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=True Trainer.save_dir=${save_dir}/MRI2CT_401lr_shuffleT_seed2"
-  "python main.py seed=321 Optim.lr=0.00001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=True Trainer.save_dir=${save_dir}/MRI2CT_401lr_shuffleT_seed3"
-
-  "python main.py seed=123 Optim.lr=0.00001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=False Trainer.save_dir=${save_dir}/MRI2CT_401lr_shuffleF_seed1"
-  "python main.py seed=213 Optim.lr=0.00001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=False Trainer.save_dir=${save_dir}/MRI2CT_401lr_shuffleF_seed2"
-  "python main.py seed=321 Optim.lr=0.00001 Trainer.name=baseline DA.source=MRI DA.target=CT DataLoader.shuffle=False Trainer.save_dir=${save_dir}/MRI2CT_401lr_shuffleF_seed3"
+  "python main.py seed=123 Optim.lr=0.0001 Trainer.name=baseline DA.source=MRI DA.target=CT   Trainer.save_dir=${save_dir}/MRI2CT_301lr_seed1"
+  "python main.py seed=123 Optim.lr=0.00001 Trainer.name=baseline DA.source=MRI DA.target=CT  Trainer.save_dir=${save_dir}/MRI2CT_401lr_seed1"
+  "python main.py seed=123 Optim.lr=0.000001 Trainer.name=baseline DA.source=MRI DA.target=CT Trainer.save_dir=${save_dir}/MRI2CT_501lr_seed1"
 
 )
 
