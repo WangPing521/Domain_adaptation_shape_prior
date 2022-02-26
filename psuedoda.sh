010e8ab6ee83f968252db581de36f8f2b75b7f62
WangPing521 <wp_scdedu@163.com>
2022-02-27 04:28:59 +0800

    bothcc

--- psuedoda.sh
+++ psuedoda.sh
@@ -15,8 +15,8 @@ declare -a StringArray=(
 # lr = 0.00001
 #------------------MR2CT
 "python pseudoDA_main.py seed=123 Optim.lr=0.00001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_seed1"
-"python pseudoDA_main.py seed=231 Optim.lr=0.00001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_seed1"
-"python pseudoDA_main.py seed=321 Optim.lr=0.00001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_seed1"
+"python pseudoDA_main.py seed=231 Optim.lr=0.00001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_seed2"
+"python pseudoDA_main.py seed=321 Optim.lr=0.00001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_seed3"
 
 
 )
