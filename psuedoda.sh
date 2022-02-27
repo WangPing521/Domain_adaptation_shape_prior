8fc42b00dcee6405d087952129679a7f0d1dbd66
WangPing521 <wp_scdedu@163.com>
2022-02-27 22:09:51 +0800

    psedulr

--- psuedoda.sh
+++ psuedoda.sh
@@ -6,7 +6,7 @@ CC_WRAPPER_PATH="CC_wrapper.sh"
 
 source $CC_WRAPPER_PATH
 
-time=5
+time=2
 account=rrg-ebrahimi
 save_dir=psuedoDA
 
@@ -14,9 +14,13 @@ declare -a StringArray=(
 # CT2MRI
 # lr = 0.00001
 #------------------MR2CT
-"python pseudoDA_main.py seed=123 Optim.lr=0.00001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_seed1"
-"python pseudoDA_main.py seed=231 Optim.lr=0.00001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_seed2"
-"python pseudoDA_main.py seed=321 Optim.lr=0.00001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_seed3"
+"python pseudoDA_main.py seed=123 Optim.lr=0.0000001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_601seed1"
+"python pseudoDA_main.py seed=231 Optim.lr=0.0000001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_601seed2"
+"python pseudoDA_main.py seed=321 Optim.lr=0.0000001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_601seed3"
+
+"python pseudoDA_main.py seed=123 Optim.lr=0.000001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_501seed1"
+"python pseudoDA_main.py seed=231 Optim.lr=0.000001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_501seed2"
+"python pseudoDA_main.py seed=321 Optim.lr=0.000001 DA.source=MRI DA.target=CT DA.batchsize_indicator=6 Trainer.save_dir=${save_dir}/pseudoDA_501seed3"
 
 
 )
