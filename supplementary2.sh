5223b6d73db2ddab55ea765b0f6590786be4f1d4
WangPing521 <wp_scdedu@163.com>
2022-03-07 07:36:57 +0800

    disp1

---
+++ supplementary2.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail
+
+CC_WRAPPER_PATH="CC_wrapper.sh"
+
+source $CC_WRAPPER_PATH
+
+time=4
+account=rrg-ebrahimi
+save_dir=NoGradS_disp1
+declare -a StringArray=(
+
+"python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=6 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True DA.align_type=MAE Scheduler.RegScheduler.max_value=0.0001    Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/disp1_21bs_401Ent_301MAEreg_seed1"
+"python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=6 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True DA.align_type=MAE Scheduler.RegScheduler.max_value=0.00001   Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/disp1_21bs_401Ent_401MAEreg_seed1"
+"python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=6 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True DA.align_type=MAE Scheduler.RegScheduler.max_value=0.00005   Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/disp1_21bs_401Ent_405MAEreg_seed1"
+
+
+"python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=6 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True DA.align_type=MAE Scheduler.RegScheduler.max_value=0.0001    Scheduler.ClusterScheduler.max_value=0.000001 Trainer.save_dir=${save_dir}/disp1_21bs_501Ent_301MAEreg_seed1"
+"python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=6 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True DA.align_type=MAE Scheduler.RegScheduler.max_value=0.00001   Scheduler.ClusterScheduler.max_value=0.000001 Trainer.save_dir=${save_dir}/disp1_21bs_501Ent_401MAEreg_seed1"
+"python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=6 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True DA.align_type=MAE Scheduler.RegScheduler.max_value=0.00005   Scheduler.ClusterScheduler.max_value=0.000001 Trainer.save_dir=${save_dir}/disp1_21bs_501Ent_405MAEreg_seed1"
+
+"python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=20 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True DA.align_type=MAE Scheduler.RegScheduler.max_value=0.0001    Scheduler.ClusterScheduler.max_value=0.0000001 Trainer.save_dir=${save_dir}/disp1_63bs_601Ent_301MAEreg_seed1"
+"python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=20 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True DA.align_type=MAE Scheduler.RegScheduler.max_value=0.00001   Scheduler.ClusterScheduler.max_value=0.0000001 Trainer.save_dir=${save_dir}/disp1_63bs_601Ent_401MAEreg_seed1"
+"python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=20 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True DA.align_type=MAE Scheduler.RegScheduler.max_value=0.00005   Scheduler.ClusterScheduler.max_value=0.0000001 Trainer.save_dir=${save_dir}/disp1_63bs_601Ent_405MAEreg_seed1"
+
+
+)
+
+for cmd in "${StringArray[@]}"; do
+  echo ${cmd}
+  CC_wrapper "${time}" "${account}" "${cmd}" 16
+
+done
