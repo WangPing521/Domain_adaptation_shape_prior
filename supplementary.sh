03d8e56719eeec022d137e2ba6913839c44c4eb1
WangPing521 <wp_scdedu@163.com>
2022-03-08 10:33:18 +0800

    visjoint

--- supplementary.sh
+++ supplementary.sh
@@ -5,7 +5,10 @@ set -e -u -o pipefail
 CC_WRAPPER_PATH="CC_wrapper.sh"
 
 source $CC_WRAPPER_PATH
-save_dir=NoGradS_visjoint
+
+time=3
+account=rrg-ebrahimi
+save_dir=NoGradS_vis
 declare -a StringArray=(
 
 "python main.py seed=123 Optim.lr=0.00001 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=20 DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False DA.align_type=MAE Scheduler.RegScheduler.max_value=0.0001 Scheduler.ClusterScheduler.max_value=0 Trainer.save_dir=${save_dir}/disp0_63bs_0Ent_301MAEreg_seed1"
