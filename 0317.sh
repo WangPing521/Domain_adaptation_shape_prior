43ef46b6f1f3f4a8fb8d9ad722d512fd088ddacf
WangPing521 <wp_scdedu@163.com>
2022-03-29 16:43:32 -0400

    scripts

--- 0317.sh
+++ 0317.sh
@@ -10,23 +10,19 @@ time=3
 account=rrg-ebrahimi
 save_dir=0329_baselines
 declare -a StringArray=(
-# batch_align: bs=7:1 21:3 35:5 42:6 56:8 63:9 98:14
+# batch_align: bs=7:1 21:3 35:5 42:6 56:8 63:9
 
-"python main.py seed=123 Optim.lr=0.00001  DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=1  Trainer.name=baseline Trainer.save_dir=${save_dir}/MRI2CT_lower_baseline7"
 "python main.py seed=123 Optim.lr=0.00001  DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=3  Trainer.name=baseline Trainer.save_dir=${save_dir}/MRI2CT_lower_baseline21"
 "python main.py seed=123 Optim.lr=0.00001  DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=5  Trainer.name=baseline Trainer.save_dir=${save_dir}/MRI2CT_lower_baseline35"
 "python main.py seed=123 Optim.lr=0.00001  DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=6  Trainer.name=baseline Trainer.save_dir=${save_dir}/MRI2CT_lower_baseline42"
 "python main.py seed=123 Optim.lr=0.00001  DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=8  Trainer.name=baseline Trainer.save_dir=${save_dir}/MRI2CT_lower_baseline56"
 "python main.py seed=123 Optim.lr=0.00001  DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=9  Trainer.name=baseline Trainer.save_dir=${save_dir}/MRI2CT_lower_baseline63"
-"python main.py seed=123 Optim.lr=0.00001  DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=14 Trainer.name=baseline Trainer.save_dir=${save_dir}/MRI2CT_lower_baseline98"
 
-"python main.py seed=123 Optim.lr=0.00001 DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=1  Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/MRI2CT_upper_baseline7"
 "python main.py seed=123 Optim.lr=0.00001 DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=3  Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/MRI2CT_upper_baseline21"
 "python main.py seed=123 Optim.lr=0.00001 DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=5  Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/MRI2CT_upper_baseline35"
 "python main.py seed=123 Optim.lr=0.00001 DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=6  Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/MRI2CT_upper_baseline42"
 "python main.py seed=123 Optim.lr=0.00001 DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=8  Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/MRI2CT_upper_baseline56"
 "python main.py seed=123 Optim.lr=0.00001 DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=9  Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/MRI2CT_upper_baseline63"
-"python main.py seed=123 Optim.lr=0.00001 DA.double_bn=False DA.source=MRI DA.target=CT DA.batchsize_indicator=14 Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/MRI2CT_upper_baseline98"
 
 )
 
