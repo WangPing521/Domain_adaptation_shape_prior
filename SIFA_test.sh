3b51ab6324e118b1553e2f5d97875c42b499a3fa
WangPing521 <wp_scdedu@163.com>
2022-05-11 14:54:49 -0400

    scpt

--- SIFA_test.sh
+++ SIFA_test.sh
@@ -26,6 +26,14 @@ declare -a StringArray=(
  Scheduler.RegScheduler_advp1.min_value=0 Scheduler.RegScheduler_advp1.max_value=0.1
  Scheduler.RegScheduler_advss.min_value=0 Scheduler.RegScheduler_advss.max_value=0.1
  Trainer.save_dir=${save_dir}/SIFA_case2"
+
+ "python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2
+ Scheduler.RegScheduler_advs.min_value=0.01  Scheduler.RegScheduler_advs.max_value=0.01
+ Scheduler.RegScheduler_cyc.min_value=1    Scheduler.RegScheduler_cyc.max_value=1
+ Scheduler.RegScheduler_seg2.min_value=0.01  Scheduler.RegScheduler_seg2.max_value=0.01
+ Scheduler.RegScheduler_advp1.min_value=0.01 Scheduler.RegScheduler_advp1.max_value=0.01
+ Scheduler.RegScheduler_advss.min_value=0.01 Scheduler.RegScheduler_advss.max_value=0.01
+ Trainer.save_dir=${save_dir}/SIFA_case3"
 )
 
 for cmd in "${StringArray[@]}"; do
