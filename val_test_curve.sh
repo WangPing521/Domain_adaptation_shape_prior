48200d06bf51502b5fc6d2cd3bfa485e8be54c79
WangPing521 <wp_scdedu@163.com>
2022-05-11 00:04:19 -0400

    crossvalidation

--- val_test_curve.sh
+++ val_test_curve.sh
@@ -8,7 +8,7 @@ source $CC_WRAPPER_PATH
 
 time=4
 account=rrg-ebrahimi
-save_dir=splits_crossvalidation_new
+save_dir=splits_crossvalidation_new4
 declare -a StringArray=(
 # align
 #"python main.py seed=123 Optim.lr=0.00001 Data.seed=12 Trainer.name=align_IndividualBN DA.double_bn=True DA.source=MRI DA.target=CT DA.batchsize_indicator=9 DA.statistic=False DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.0001 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/fold1_run1"
