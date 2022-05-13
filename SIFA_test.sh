5e465c5a619020148b81068ee05c9f714aa4da8b
WangPing521 <wp_scdedu@163.com>
2022-05-13 14:26:57 -0400

    lr_weighttunning

--- SIFA_test.sh
+++ SIFA_test.sh
@@ -8,18 +8,16 @@ source $CC_WRAPPER_PATH
 
 time=4
 account=rrg-ebrahimi
-save_dir=SIFA_0509
+save_dir=SIFA_0512
 declare -a StringArray=(
 # align
-"python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=1 weights.seg_weight=1 weights.disc_weight=0.1 Trainer.save_dir=${save_dir}/SIFA_c1s1d01"
-"python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=1 weights.seg_weight=1 weights.disc_weight=0.01 Trainer.save_dir=${save_dir}/SIFA_c1s1d101"
 "python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=1 weights.seg_weight=1 weights.disc_weight=0.001 Trainer.save_dir=${save_dir}/SIFA_c1s1d201"
 "python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=1 weights.seg_weight=1 weights.disc_weight=0.0001 Trainer.save_dir=${save_dir}/SIFA_c1s1d301"
+"python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=1 weights.seg_weight=1 weights.disc_weight=0.00001 Trainer.save_dir=${save_dir}/SIFA_c1s1d401"
 
-"python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=10 weights.seg_weight=1 weights.disc_weight=0.1 Trainer.save_dir=${save_dir}/SIFA_c10s1d01"
-"python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=10 weights.seg_weight=1 weights.disc_weight=0.01 Trainer.save_dir=${save_dir}/SIFA_c10s1d101"
 "python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=10 weights.seg_weight=1 weights.disc_weight=0.001 Trainer.save_dir=${save_dir}/SIFA_c10s1d201"
 "python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=10 weights.seg_weight=1 weights.disc_weight=0.0001 Trainer.save_dir=${save_dir}/SIFA_c10s1d301"
+"python SIFA_main.py seed=123 DA.source=MRI DA.target=CT DA.batchsize_indicator=2 weights.cyc_weight=10 weights.seg_weight=1 weights.disc_weight=0.00001 Trainer.save_dir=${save_dir}/SIFA_c10s1d401"
 
 )
 
