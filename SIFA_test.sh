6394203e163c30bbeb7545155c9a9b6ab242b323
WangPing521 <wp_scdedu@163.com>
2022-06-03 22:58:26 -0400

    sifabs

--- SIFA_test.sh
+++ SIFA_test.sh
@@ -10,18 +10,18 @@ time=6
 account=rrg-ebrahimi
 save_dir=0603_SIFA
 declare -a StringArray=(
-#todo check lr
-"python SIFA_main.py seed=10 Data.kfold=1 Optim.lr=0.00001 Optim.disc_lr=0.000002 DataLoader.batch_size=63 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_502dlr_c1s2d205_fold1"
-"python SIFA_main.py seed=10 Data.kfold=2 Optim.lr=0.00001 Optim.disc_lr=0.000002 DataLoader.batch_size=63 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_502dlr_c1s2d205_fold2"
-"python SIFA_main.py seed=10 Data.kfold=3 Optim.lr=0.00001 Optim.disc_lr=0.000002 DataLoader.batch_size=63 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_502dlr_c1s2d205_fold3"
+#todo check lr  (note: maybe the batch size is too large for this method, leading to out of memory)
+"python SIFA_main.py seed=10 Data.kfold=1 Optim.lr=0.00001 Optim.disc_lr=0.000002 DataLoader.batch_size=32 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_502dlr_c1s2d205_fold1"
+"python SIFA_main.py seed=10 Data.kfold=2 Optim.lr=0.00001 Optim.disc_lr=0.000002 DataLoader.batch_size=32 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_502dlr_c1s2d205_fold2"
+"python SIFA_main.py seed=10 Data.kfold=3 Optim.lr=0.00001 Optim.disc_lr=0.000002 DataLoader.batch_size=32 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_502dlr_c1s2d205_fold3"
 
-"python SIFA_main.py seed=10 Data.kfold=1 Optim.lr=0.00001 Optim.disc_lr=0.000001 DataLoader.batch_size=63 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_501lr_c1s2d205_fold1"
-"python SIFA_main.py seed=10 Data.kfold=2 Optim.lr=0.00001 Optim.disc_lr=0.000001 DataLoader.batch_size=63 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_501lr_c1s2d205_fold2"
-"python SIFA_main.py seed=10 Data.kfold=3 Optim.lr=0.00001 Optim.disc_lr=0.000001 DataLoader.batch_size=63 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_501lr_c1s2d205_fold3"
+"python SIFA_main.py seed=10 Data.kfold=1 Optim.lr=0.00001 Optim.disc_lr=0.000001 DataLoader.batch_size=32 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_501lr_c1s2d205_fold1"
+"python SIFA_main.py seed=10 Data.kfold=2 Optim.lr=0.00001 Optim.disc_lr=0.000001 DataLoader.batch_size=32 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_501lr_c1s2d205_fold2"
+"python SIFA_main.py seed=10 Data.kfold=3 Optim.lr=0.00001 Optim.disc_lr=0.000001 DataLoader.batch_size=32 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_501lr_c1s2d205_fold3"
 
-"python SIFA_main.py seed=10 Data.kfold=1 Optim.lr=0.00001 Optim.disc_lr=0.0000001 DataLoader.batch_size=63 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_601dlr_c1s2d205_fold1"
-"python SIFA_main.py seed=10 Data.kfold=2 Optim.lr=0.00001 Optim.disc_lr=0.0000001 DataLoader.batch_size=63 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_601dlr_c1s2d205_fold2"
-"python SIFA_main.py seed=10 Data.kfold=3 Optim.lr=0.00001 Optim.disc_lr=0.0000001 DataLoader.batch_size=63 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_601dlr_c1s2d205_fold3"
+"python SIFA_main.py seed=10 Data.kfold=1 Optim.lr=0.00001 Optim.disc_lr=0.0000001 DataLoader.batch_size=32 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_601dlr_c1s2d205_fold1"
+"python SIFA_main.py seed=10 Data.kfold=2 Optim.lr=0.00001 Optim.disc_lr=0.0000001 DataLoader.batch_size=32 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_601dlr_c1s2d205_fold2"
+"python SIFA_main.py seed=10 Data.kfold=3 Optim.lr=0.00001 Optim.disc_lr=0.0000001 DataLoader.batch_size=32 weights.cyc_weight=1 weights.seg_weight=2 weights.disc_weight=0.005 Trainer.save_dir=${save_dir}/SIFA_601dlr_c1s2d205_fold3"
 
 #todo check batch size, weight_cyc, weight_seg, weight_disc
 
--- alignment_weight_disp.sh
+++ alignment_weight_disp.sh
@@ -6,22 +6,13 @@ CC_WRAPPER_PATH="CC_wrapper.sh"
 
 source $CC_WRAPPER_PATH
 
-time=3
+time=4
 account=rrg-ebrahimi
-save_dir=0603_alignment_weight_disp
+save_dir=0603_alignment_weight_disp1
 
 declare -a StringArray=(
 
 #todo disp=0, output layer, resolution=256, ent+align
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=1 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.00001 Scheduler.ClusterScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/disp0_301Ent_401joint_fold1"
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=2 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.00001 Scheduler.ClusterScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/disp0_301Ent_401joint_fold2"
-
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=1 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/disp0_301Ent_405joint_fold1"
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=2 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/disp0_301Ent_405joint_fold2"
-
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=1 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.0001 Scheduler.ClusterScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/disp0_301Ent_301joint_fold1"
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=2 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.0001 Scheduler.ClusterScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/disp0_301Ent_301joint_fold2"
-
 "python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=1 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.0005 Scheduler.ClusterScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/disp0_301Ent_305joint_fold1"
 "python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=2 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.0005 Scheduler.ClusterScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/disp0_301Ent_305joint_fold2"
 
@@ -32,9 +23,6 @@ declare -a StringArray=(
 "python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=2 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.005 Scheduler.ClusterScheduler.max_value=0.0001 Trainer.save_dir=${save_dir}/disp0_301Ent_205joint_fold2"
 
 #----
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=1 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.00001 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/disp0_401Ent_401joint_fold1"
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=2 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.00001 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/disp0_401Ent_401joint_fold2"
-
 "python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=1 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/disp0_401Ent_405joint_fold1"
 "python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=2 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/disp0_401Ent_405joint_fold2"
 
