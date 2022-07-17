fb8166a89194042372af092c1b158954367c6605
WangPing521 <wp_scdedu@163.com>
2022-07-16 20:06:19 -0400

    mtuda

--- MTUDA_0710.sh
+++ MTUDA_0710.sh
@@ -8,40 +8,24 @@ source $CC_WRAPPER_PATH
 
 time=2
 account=def-chdesa
-save_dir=0715_MTUDA_runs_lr301_lr501
+save_dir=0715_MTUDA_runs_mse
 declare -a StringArray=(
-"python MTUDA_main.py seed=10 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/301lr_lkd1_cons101_seed1"
-"python MTUDA_main.py seed=20 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/301lr_lkd1_cons101_seed2"
-"python MTUDA_main.py seed=30 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/301lr_lkd1_cons101_seed3"
+"python MTUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/lkd1_cons101_seed1"
+"python MTUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/lkd1_cons101_seed2"
+"python MTUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/lkd1_cons101_seed3"
 ##
-"python MTUDA_main.py seed=10 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.1  Trainer.save_dir=${save_dir}/301lr_lk01_cons01_seed1"
-"python MTUDA_main.py seed=20 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.1  Trainer.save_dir=${save_dir}/301lr_lk01_cons01_seed2"
-"python MTUDA_main.py seed=30 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.1  Trainer.save_dir=${save_dir}/301lr_lk01_cons01_seed3"
+"python MTUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.1  Trainer.save_dir=${save_dir}/lk01_cons01_seed1"
+"python MTUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.1  Trainer.save_dir=${save_dir}/lk01_cons01_seed2"
+"python MTUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.1  Trainer.save_dir=${save_dir}/lk01_cons01_seed3"
 
-"python MTUDA_main.py seed=10 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.05 Trainer.save_dir=${save_dir}/301lr_lk01_cons105_seed1"
-"python MTUDA_main.py seed=20 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.05 Trainer.save_dir=${save_dir}/301lr_lk01_cons105_seed2"
-"python MTUDA_main.py seed=30 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.05 Trainer.save_dir=${save_dir}/301lr_lk01_cons105_seed3"
+"python MTUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.05 Trainer.save_dir=${save_dir}/lk01_cons105_seed1"
+"python MTUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.05 Trainer.save_dir=${save_dir}/lk01_cons105_seed2"
+"python MTUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.05 Trainer.save_dir=${save_dir}/lk01_cons105_seed3"
 
-"python MTUDA_main.py seed=10 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/301lr_lk01_cons101_seed1"
-"python MTUDA_main.py seed=20 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/301lr_lk01_cons101_seed2"
-"python MTUDA_main.py seed=30 Optim.lr=0.0001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/301lr_lk01_cons101_seed3"
+"python MTUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/lk01_cons101_seed1"
+"python MTUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/lk01_cons101_seed2"
+"python MTUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/lk01_cons101_seed3"
 
-#=============
-"python MTUDA_main.py seed=10 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/501lr_lkd1_cons101_seed1"
-"python MTUDA_main.py seed=20 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/501lr_lkd1_cons101_seed2"
-"python MTUDA_main.py seed=30 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/501lr_lkd1_cons101_seed3"
-##
-"python MTUDA_main.py seed=10 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.1  Trainer.save_dir=${save_dir}/501lr_lk01_cons01_seed1"
-"python MTUDA_main.py seed=20 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.1  Trainer.save_dir=${save_dir}/501lr_lk01_cons01_seed2"
-"python MTUDA_main.py seed=30 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.1  Trainer.save_dir=${save_dir}/501lr_lk01_cons01_seed3"
-
-"python MTUDA_main.py seed=10 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.05 Trainer.save_dir=${save_dir}/501lr_lk01_cons105_seed1"
-"python MTUDA_main.py seed=20 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.05 Trainer.save_dir=${save_dir}/501lr_lk01_cons105_seed2"
-"python MTUDA_main.py seed=30 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.05 Trainer.save_dir=${save_dir}/501lr_lk01_cons105_seed3"
-
-"python MTUDA_main.py seed=10 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/501lr_lk01_cons101_seed1"
-"python MTUDA_main.py seed=20 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/501lr_lk01_cons101_seed2"
-"python MTUDA_main.py seed=30 Optim.lr=0.000001 DataLoader.batch_size=63 weights.lkd_weight=0.1 weights.consistency=0.01 Trainer.save_dir=${save_dir}/501lr_lk01_cons101_seed3"
 
 )
 
--- trainers/MTUDA_trainer.py
+++ trainers/MTUDA_trainer.py
@@ -191,8 +191,8 @@ class MTUDA_trainer:
             pred_s_ema = self.source_ema_model(S_img).softmax(1)
             pred_t2s_ema = self.source_ema_model(T2S_img).softmax(1)
         # semantic
-        lkd_loss = self.kl(pred_s_0, pred_s_ema) + self.kl(pred_t2s_0, pred_t2s_ema)
-
+        # lkd_loss = self.kl(pred_s_0, pred_s_ema) + self.kl(pred_t2s_0, pred_t2s_ema)
+        lkd_loss = F.mse_loss(pred_s_0, pred_s_ema) + F.mse_loss(pred_t2s_0, pred_t2s_ema)
         with torch.no_grad():
             pred_t_ema = self.target_ema_model(T_img).softmax(1)
             pred_s2t_ema = self.target_ema_model(S2T_img).softmax(1)
