1b8c1cd7dbcccc4fc35925e3fa9518e51ce76807
WangPing521 <wp_scdedu@163.com>
2022-07-19 15:26:40 -0400

    ponitbash

--- pointUDA_0718.sh
+++ pointUDA_0718.sh
@@ -6,27 +6,27 @@ CC_WRAPPER_PATH="CC_wrapper.sh"
 
 source $CC_WRAPPER_PATH
 
-time=2
+time=4
 account=def-chdesa
 save_dir=0718_pointUDA_mmwhs
 declare -a StringArray=(
 #-----------
 
 "python PointCloudUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.001 Trainer.save_dir=${save_dir}/bs63_201dis_seed1"
-"python PointCloudUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.001 Trainer.save_dir=${save_dir}/bs63_201dis_seed2"
-"python PointCloudUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.001 Trainer.save_dir=${save_dir}/bs63_201dis_seed3"
+#"python PointCloudUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.001 Trainer.save_dir=${save_dir}/bs63_201dis_seed2"
+#"python PointCloudUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.001 Trainer.save_dir=${save_dir}/bs63_201dis_seed3"
 
 "python PointCloudUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.01 Trainer.save_dir=${save_dir}/bs63_101dis_seed1"
-"python PointCloudUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.01 Trainer.save_dir=${save_dir}/bs63_101dis_seed2"
-"python PointCloudUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.01 Trainer.save_dir=${save_dir}/bs63_101dis_seed3"
+#"python PointCloudUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.01 Trainer.save_dir=${save_dir}/bs63_101dis_seed2"
+#"python PointCloudUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.01 Trainer.save_dir=${save_dir}/bs63_101dis_seed3"
 
 "python PointCloudUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.2 Trainer.save_dir=${save_dir}/bs63_02dis_seed1"
-"python PointCloudUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.2 Trainer.save_dir=${save_dir}/bs63_02dis_seed2"
-"python PointCloudUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.2 Trainer.save_dir=${save_dir}/bs63_02dis_seed3"
+#"python PointCloudUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.2 Trainer.save_dir=${save_dir}/bs63_02dis_seed2"
+#"python PointCloudUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.2 Trainer.save_dir=${save_dir}/bs63_02dis_seed3"
 
 "python PointCloudUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.5 Trainer.save_dir=${save_dir}/bs63_05dis_seed1"
-"python PointCloudUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.5 Trainer.save_dir=${save_dir}/bs63_05dis_seed2"
-"python PointCloudUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.5 Trainer.save_dir=${save_dir}/bs63_05dis_seed3"
+#"python PointCloudUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.5 Trainer.save_dir=${save_dir}/bs63_05dis_seed2"
+#"python PointCloudUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights=0.5 Trainer.save_dir=${save_dir}/bs63_05dis_seed3"
 
 
 )
