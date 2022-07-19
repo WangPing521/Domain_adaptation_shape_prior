897a25ba6ea7bb38ad525648a044ec9ccbc24aeb
WangPing521 <wp_scdedu@163.com>
2022-07-19 17:14:47 -0400

    deviceerror

--- trainers/pointcloudUDA_trainer.py
+++ trainers/pointcloudUDA_trainer.py
@@ -111,6 +111,7 @@ class pointCloudUDA_trainer:
 
         with fix_all_seed_within_context(self._config['seed']):
             self.point_net = PointNet(num_points=300, ext=False)
+            self.point_net.to(device)
         self.optimizer.add_param_group({'params': self.point_net.parameters()})
 
         self.scheduler = scheduler
@@ -163,7 +164,6 @@ class pointCloudUDA_trainer:
 
     def to(self, device):
         self.model.to(device=device)
-        self.point_net.to(device=device)
         self.discriminator_1.to(device=device)
         self.discriminator_2.to(device=device)
         self.discriminator_3.to(device=device)
