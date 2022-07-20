da661c63dd1935c968c596612d7a5b8d300de695
WangPing521 <wp_scdedu@163.com>
2022-07-20 16:23:12 -0400

    new_prostatebash

--- state2mise_script.sh
+++ state2mise_script.sh
@@ -10,11 +10,6 @@ time=3
 account=def-chdesa
 save_dir=0720_prostate_align_new
 declare -a StringArray=(
-#disp0
-#"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=prostate Data_input.num_class=2 Data.kfold=0 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.000001 Scheduler.ClusterScheduler.max_value=0.000001 Trainer.save_dir=${save_dir}/disp0_501Ent_501joint_seed1"
-#"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=prostate Data_input.num_class=2 Data.kfold=0 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.0000001 Scheduler.ClusterScheduler.max_value=0.000001 Trainer.save_dir=${save_dir}/disp0_501Ent_601joint_seed1"
-#"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=prostate Data_input.num_class=2 Data.kfold=0 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.0000005 Scheduler.ClusterScheduler.max_value=0.000001 Trainer.save_dir=${save_dir}/disp0_501Ent_605joint_seed1"
-
 # disp1
 #---
 "python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=prostate Data_input.num_class=2 Data.kfold=0 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=True Scheduler.RegScheduler.max_value=0.0000001 Scheduler.ClusterScheduler.max_value=0.000001 Trainer.save_dir=${save_dir}/disp1_501Ent_601joint_seed1"
