497ac4a29fc70b8896b673328687fdcee52726da
WangPing521 <wp_scdedu@163.com>
2022-07-20 12:55:07 -0400

    prostateseg

--- state2mise_script.sh
+++ state2mise_script.sh
@@ -8,7 +8,7 @@ source $CC_WRAPPER_PATH
 
 time=3
 account=rrg-ebrahimi
-save_dir=0720_prostate_disp0
+save_dir=0720_prostate_align
 declare -a StringArray=(
 #disp0
 "python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=prostate Data_input.num_class=2 Data.kfold=0 Trainer.name=align_IndividualBN DA.double_bn=True DA.batchsize_indicator=9 DA.statistic=True DA.align_layer.name=Deconv_1x1 DA.multi_scale=1 DA.displacement=False Scheduler.RegScheduler.max_value=0.00001 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/disp0_401Ent_401joint_seed1"
