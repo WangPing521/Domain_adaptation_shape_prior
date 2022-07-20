96eea412011b5c12e2931f4751637951ff915ec3
WangPing521 <wp_scdedu@163.com>
2022-07-20 10:08:32 -0400

    workerdecrease

--- arch/pointNet.py
+++ arch/pointNet.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.metrics import pairwise_distances
 from torch.nn import functional as F
 
-
 class PointNet(nn.Module):
     def __init__(self, num_points=300, fc_inch=121, conv_inch=256, ext=False):
         super().__init__()
@@ -11,9 +10,9 @@ class PointNet(nn.Module):
         self.ReLU = nn.LeakyReLU(inplace=True)
         # Final convolution is initialized differently form the rest
         if ext:
-            self.conv1 = nn.Conv2d(conv_inch, conv_inch * 2, kernel_size=3, padding=1)
-            self.conv2 = nn.Conv2d(conv_inch * 2, conv_inch, kernel_size=3, padding=1)
-        self.final_conv = nn.Conv2d(conv_inch, self.num_points, kernel_size=6)
+            self.conv1 = nn.Conv2d(conv_inch, conv_inch * 2, kernel_size=(3,3), padding=(1,1))
+            self.conv2 = nn.Conv2d(conv_inch * 2, conv_inch, kernel_size=(3,3), padding=(1,1))
+        self.final_conv = nn.Conv2d(conv_inch, self.num_points, kernel_size=(6,6))
         self.final_fc = nn.Linear(fc_inch, 3)
         self._ext = ext
 
