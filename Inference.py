4cb763c9b454d4537bcec59028d4f2107da05748
WangPing521 <wp_scdedu@163.com>
2022-07-24 18:09:40 -0400

    mmwhsmtudavis

--- Inference.py
+++ Inference.py
@@ -29,26 +29,26 @@ def save_images(segs: Tensor, names: Iterable[str], root: Union[str, Path], mode
 
 
 dataset = 'mmwhs'
-double_bn = True
+double_bn = False
 # mmwhs: 5
 # prostate: 2
 Smodel = UNet(num_classes=5, input_dim=1)
 if double_bn:
     Smodel = convert2TwinBN(Smodel)
-Smodel = Smodel.eval()
-weight = f'../../PHD_documents/papers_work/domain_adaptation/redoresults/check_segs/disp1_401Ent_301MAEregjoint63_seed3/last.pth'
+weight = f'../../PHD_documents/papers_work/domain_adaptation/MMWHS/SOTA/cyc1_Tcyc05_seg2_disSeg305_dis101_seed1/last.pth'
 new_state_dict = OrderedDict()
 state_dict = torch.load(weight, map_location=torch.device('cpu'))
 Smodel.load_state_dict(state_dict.get('model'))
+Smodel = Smodel.eval()
 
 if dataset == 'mmwhs':
     target_handler = mmWHSCTInterface(seed=12)
 else:
     target_handler = PromiseInterface(seed=12)
-target_handler.compile_dataloader_params(batch_size=1, val_batch_size=1,shuffle=False,num_workers=1,pin_memory=False)
+target_handler.compile_dataloader_params(batch_size=10, val_batch_size=10,shuffle=False,num_workers=1,pin_memory=False)
 
 with fix_all_seed_within_context(seed=12):
-    trainT_loader, valT_loader = target_handler.DataLoaders(
+    trainT_loader, valT_loader, test_loader = target_handler.DataLoaders(
         train_transform=None,
         val_transform=None,
         group_val=False,
@@ -57,7 +57,7 @@ with fix_all_seed_within_context(seed=12):
     )
 
 switch_bn = _switch_bn if True else nullcontext
-for batch_idT, data_T in enumerate(valT_loader):
+for batch_idT, data_T in enumerate(test_loader):
     imageT, targetT, filenameT = (
         data_T[0][0].to('cpu'),
         data_T[0][1].to('cpu'),
@@ -69,7 +69,10 @@ for batch_idT, data_T in enumerate(valT_loader):
     else:
         preds_T = Smodel(imageT).softmax(1)
 
-    save_images(preds_T.max(1)[1], names=filenameT, root='../../PHD_documents/papers_work/domain_adaptation/redoresults/check_segs/disp1_401Ent_301MAEregjoint63_seed3/segs', mode='predictions',
+    # save_images(targetT.squeeze(1), names=filenameT, root='../../PHD_documents/papers_work/domain_adaptation/test_mmwhs',
+    #             mode='gt',
+    #             iter=100)
+    save_images(preds_T.max(1)[1], names=filenameT, root='../../PHD_documents/papers_work/domain_adaptation/test_mmwhs/sifa', mode='predictions',
             iter=100)
 
 
--- SOTA_mmwhs.sh
+++ SOTA_mmwhs.sh
@@ -6,49 +6,49 @@ CC_WRAPPER_PATH="CC_wrapper.sh"
 
 source $CC_WRAPPER_PATH
 
-time=4
-account=rrg-ebrahimi
-save_dir=SOTA_mmwhs
+time=6
+account=def-chdesa
+save_dir=SOTA_mmwhs_MTUDA
 
 declare -a StringArray=(
 
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=baseline Trainer.save_dir=${save_dir}/lower_baseline_seed1"
-"python main.py seed=20 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=baseline Trainer.save_dir=${save_dir}/lower_baseline_seed2"
-"python main.py seed=30 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=baseline Trainer.save_dir=${save_dir}/lower_baseline_seed3"
-
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/upper_baseline_seed1"
-"python main.py seed=20 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/upper_baseline_seed2"
-"python main.py seed=30 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/upper_baseline_seed3"
-
-#EntDA
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=0 DA.batchsize_indicator=9 Trainer.name=entda DA.double_bn=True Scheduler.RegScheduler.max_value=0.00000001 Trainer.save_dir=${save_dir}/entDA63_701reg_seed1"
-"python main.py seed=20 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=0 DA.batchsize_indicator=9 Trainer.name=entda DA.double_bn=True Scheduler.RegScheduler.max_value=0.00000001 Trainer.save_dir=${save_dir}/entDA63_701reg_seed2"
-"python main.py seed=30 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=0 DA.batchsize_indicator=9 Trainer.name=entda DA.double_bn=True Scheduler.RegScheduler.max_value=0.00000001 Trainer.save_dir=${save_dir}/entDA63_701reg_seed3"
-
-#pseudo_DA
-"python pseudoDA_main.py seed=10 Data_input.dataset=mmwhs Data.kfold=0 Optim.lr=0.00001 DA.batchsize_indicator=9 Trainer.save_dir=${save_dir}/pseudoDA_seed1"
-"python pseudoDA_main.py seed=20 Data_input.dataset=mmwhs Data.kfold=0 Optim.lr=0.00001 DA.batchsize_indicator=9 Trainer.save_dir=${save_dir}/pseudoDA_seed2"
-"python pseudoDA_main.py seed=30 Data_input.dataset=mmwhs Data.kfold=0 Optim.lr=0.00001 DA.batchsize_indicator=9 Trainer.save_dir=${save_dir}/pseudoDA_seed3"
-
-#Entprior
-"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=True Data.kfold=0 Trainer.name=priorbased DA.batchsize_indicator=9 Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/EntPrior_401Ent_405prior_seed1"
-"python main.py seed=20 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=True Data.kfold=0 Trainer.name=priorbased DA.batchsize_indicator=9 Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/EntPrior_401Ent_405prior_seed2"
-"python main.py seed=30 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=True Data.kfold=0 Trainer.name=priorbased DA.batchsize_indicator=9 Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/EntPrior_401Ent_405prior_seed3"
-
-#SIFA
-# cyc_weight: 1 2
-# cyc_Tweight: 0.5  1
-# seg_weight: 1 2 4
-# discSeg_weight: 0.0001 0.0005 0.001 0.005 0.1
-# disc_weight: 0.0001 0.0005 0.001 0.005 0.1
-"python SIFA_main.py seed=10 Data.kfold=0 Optim.lr=0.000001 Optim.disc_lr=0.000005 DataLoader.batch_size=32 weights.cyc_weight=1 weights.cyc_Tweight=0.5 weights.seg_weight=2 weights.discSeg_weight=0.0005 weights.disc_weight=0.01 Trainer.save_dir=${save_dir}/cyc1_Tcyc05_seg2_disSeg305_dis101_seed1"
-"python SIFA_main.py seed=20 Data.kfold=0 Optim.lr=0.000001 Optim.disc_lr=0.000005 DataLoader.batch_size=32 weights.cyc_weight=1 weights.cyc_Tweight=0.5 weights.seg_weight=2 weights.discSeg_weight=0.0005 weights.disc_weight=0.01 Trainer.save_dir=${save_dir}/cyc1_Tcyc05_seg2_disSeg305_dis101_seed2"
-"python SIFA_main.py seed=30 Data.kfold=0 Optim.lr=0.000001 Optim.disc_lr=0.000005 DataLoader.batch_size=32 weights.cyc_weight=1 weights.cyc_Tweight=0.5 weights.seg_weight=2 weights.discSeg_weight=0.0005 weights.disc_weight=0.01 Trainer.save_dir=${save_dir}/cyc1_Tcyc05_seg2_disSeg305_dis101_seed3"
-
+#"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=baseline Trainer.save_dir=${save_dir}/lower_baseline_seed1"
+#"python main.py seed=20 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=baseline Trainer.save_dir=${save_dir}/lower_baseline_seed2"
+#"python main.py seed=30 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=baseline Trainer.save_dir=${save_dir}/lower_baseline_seed3"
+#
+#"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/upper_baseline_seed1"
+#"python main.py seed=20 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/upper_baseline_seed2"
+#"python main.py seed=30 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=False DA.batchsize_indicator=9 Data.kfold=0 Trainer.name=upperbaseline Trainer.save_dir=${save_dir}/upper_baseline_seed3"
+#
+##EntDA
+#"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=0 DA.batchsize_indicator=9 Trainer.name=entda DA.double_bn=True Scheduler.RegScheduler.max_value=0.00000001 Trainer.save_dir=${save_dir}/entDA63_701reg_seed1"
+#"python main.py seed=20 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=0 DA.batchsize_indicator=9 Trainer.name=entda DA.double_bn=True Scheduler.RegScheduler.max_value=0.00000001 Trainer.save_dir=${save_dir}/entDA63_701reg_seed2"
+#"python main.py seed=30 Optim.lr=0.00001 Data_input.dataset=mmwhs Data.kfold=0 DA.batchsize_indicator=9 Trainer.name=entda DA.double_bn=True Scheduler.RegScheduler.max_value=0.00000001 Trainer.save_dir=${save_dir}/entDA63_701reg_seed3"
+#
+##pseudo_DA
+#"python pseudoDA_main.py seed=10 Data_input.dataset=mmwhs Data.kfold=0 Optim.lr=0.00001 DA.batchsize_indicator=9 Trainer.save_dir=${save_dir}/pseudoDA_seed1"
+#"python pseudoDA_main.py seed=20 Data_input.dataset=mmwhs Data.kfold=0 Optim.lr=0.00001 DA.batchsize_indicator=9 Trainer.save_dir=${save_dir}/pseudoDA_seed2"
+#"python pseudoDA_main.py seed=30 Data_input.dataset=mmwhs Data.kfold=0 Optim.lr=0.00001 DA.batchsize_indicator=9 Trainer.save_dir=${save_dir}/pseudoDA_seed3"
+#
+##Entprior
+#"python main.py seed=10 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=True Data.kfold=0 Trainer.name=priorbased DA.batchsize_indicator=9 Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/EntPrior_401Ent_405prior_seed1"
+#"python main.py seed=20 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=True Data.kfold=0 Trainer.name=priorbased DA.batchsize_indicator=9 Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/EntPrior_401Ent_405prior_seed2"
+#"python main.py seed=30 Optim.lr=0.00001 Data_input.dataset=mmwhs DA.double_bn=True Data.kfold=0 Trainer.name=priorbased DA.batchsize_indicator=9 Scheduler.RegScheduler.max_value=0.00005 Scheduler.ClusterScheduler.max_value=0.00001 Trainer.save_dir=${save_dir}/EntPrior_401Ent_405prior_seed3"
+#
+##SIFA
+## cyc_weight: 1 2
+## cyc_Tweight: 0.5  1
+## seg_weight: 1 2 4
+## discSeg_weight: 0.0001 0.0005 0.001 0.005 0.1
+## disc_weight: 0.0001 0.0005 0.001 0.005 0.1
+#"python SIFA_main.py seed=10 Data.kfold=0 Optim.lr=0.000001 Optim.disc_lr=0.000005 DataLoader.batch_size=32 weights.cyc_weight=1 weights.cyc_Tweight=0.5 weights.seg_weight=2 weights.discSeg_weight=0.0005 weights.disc_weight=0.01 Trainer.save_dir=${save_dir}/cyc1_Tcyc05_seg2_disSeg305_dis101_seed1"
+#"python SIFA_main.py seed=20 Data.kfold=0 Optim.lr=0.000001 Optim.disc_lr=0.000005 DataLoader.batch_size=32 weights.cyc_weight=1 weights.cyc_Tweight=0.5 weights.seg_weight=2 weights.discSeg_weight=0.0005 weights.disc_weight=0.01 Trainer.save_dir=${save_dir}/cyc1_Tcyc05_seg2_disSeg305_dis101_seed2"
+#"python SIFA_main.py seed=30 Data.kfold=0 Optim.lr=0.000001 Optim.disc_lr=0.000005 DataLoader.batch_size=32 weights.cyc_weight=1 weights.cyc_Tweight=0.5 weights.seg_weight=2 weights.discSeg_weight=0.0005 weights.disc_weight=0.01 Trainer.save_dir=${save_dir}/cyc1_Tcyc05_seg2_disSeg305_dis101_seed3"
+#
 #MTUDA
-"python MTUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight.max_value=1 weights.consistency.max_value=0.5 Trainer.checkpoint_path=runs/${save_dir}/bs63_lkd1_cons05_seed1 Trainer.save_dir=${save_dir}/bs63_lkd1_cons05_seed1"
-"python MTUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight.max_value=1 weights.consistency.max_value=0.5 Trainer.checkpoint_path=runs/${save_dir}/bs63_lkd1_cons05_seed2 Trainer.save_dir=${save_dir}/bs63_lkd1_cons05_seed2"
-"python MTUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight.max_value=1 weights.consistency.max_value=0.5 Trainer.checkpoint_path=runs/${save_dir}/bs63_lkd1_cons05_seed3 Trainer.save_dir=${save_dir}/bs63_lkd1_cons05_seed3"
+"python MTUDA_main.py seed=10 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight.max_value=1 weights.consistency.max_value=0.5 Trainer.save_dir=${save_dir}/bs63_lkd1_cons05_seed1"
+"python MTUDA_main.py seed=20 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight.max_value=1 weights.consistency.max_value=0.5 Trainer.save_dir=${save_dir}/bs63_lkd1_cons05_seed2"
+"python MTUDA_main.py seed=30 Optim.lr=0.00001 DataLoader.batch_size=63 weights.lkd_weight.max_value=1 weights.consistency.max_value=0.5 Trainer.save_dir=${save_dir}/bs63_lkd1_cons05_seed3"
 
 )
 
